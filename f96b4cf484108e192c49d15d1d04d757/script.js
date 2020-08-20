e71d34e44996a4d7d123aaf222b50a70ff5d98de
Anastasia Davydenko <latysheva93@list.ru>
2020-08-20 21:22:36 +0700

    Код переписан в соотв-ии с ES6

---
+++ f96b4cf484108e192c49d15d1d04d757/script.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const employers = ['АртеМ', 'максим', 'Владимир', 'сергей', 'НикиТа', 'евГений', ' Дарья', ' ', 'виктория ', 'ЕкаТерина', '', ' Андрей ', 'КИРИЛЛ'],
+	nameCourse = 'Базовый React';
+let command = [];
+
+employers.forEach(elem => {
+	if(elem.trim()) {
+		command.push(elem);
+	}
+});
+
+command = command.map(elem => {
+	elem = elem.toLowerCase().trim();
+	elem = elem[0].toUpperCase() + elem.slice(1);
+	return elem;
+});
+
+const data = {
+	cash: [3, 8, 3],
+	react: ['JSX', 'components', 'props', 'state', 'hooks'],
+	add: ['styled-components', 'firebase']
+};
+
+function calcCash() {
+	const everyCash = [].concat(...arguments),
+		total = everyCash.reduce((acc, cur) => acc + cur);
+	return total;
+}
+
+const lesson = calcCash(data.cash);
+
+const makeBusiness = (director, allModule, gang, course, teacher = 'Максим') => {
+	console.log(
+`Стартуем новый курс: ${course}. Владелец: ${director}. Преподаватель: ${teacher}. Всего уроков: ${allModule}.
+Команда Академии: ${gang}.
+Первое, что изучим будет ${data.react[0]}. Он очень похож на HTML!
+Технологии, которые мы изучим: ${data.react} и другие. `);
+
+};
+
+makeBusiness(...['Артем', lesson, command, nameCourse]);
+
